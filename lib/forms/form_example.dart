d75bf1626c4e44f6eaa5fd32e8f5fe996691958e
shady samara <shadysamara@shadys-MacBook-Pro.local>
2021-06-28 12:17:09 +0300

    forms

--- lib/forms/form_example.dart
+++ lib/forms/form_example.dart
@@ -57,50 +57,67 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+/*
+1- replace textfield with textformfield
+2- wrap all your textFotmFields with Form Widget
+3- define validator and onsave for each textFormField
+4- defile global key 
+5- assign the defined global key for the form
+6- excute globalKey.currentState.validate() 
+7- excute globalKey.currentState.save() 
+
+
+
+*/
+
   String email = '';
 
   String password;
 
   bool isAccepted = false;
-
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
   sendForm() {
-    if (isAccepted) {
-      Navigator.push(context, MaterialPageRoute(builder: (context) {
-        return HomePage(
-          email: this.email,
-          password: this.password,
-        );
-      }));
-    } else {
-      showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              title: Text('Warning'),
-              content:
-                  Text('you have to accept our conditions before sending data'),
-              actions: [
-                TextButton(
-                    onPressed: () {
-                      this.isAccepted = true;
-                      setState(() {});
-                      Navigator.pushReplacement(context,
-                          MaterialPageRoute(builder: (context) {
-                        return HomePage(
-                          email: this.email,
-                          password: this.password,
-                        );
-                      }));
-                    },
-                    child: Text('Accept')),
-                TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text('Cancle')),
-              ],
-            );
-          });
+    bool validationResult = formKey.currentState.validate();
+    if (validationResult) {
+      formKey.currentState.save();
+      if (isAccepted) {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return HomePage(
+            email: this.email,
+            password: this.password,
+          );
+        }));
+      } else {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('Warning'),
+                content: Text(
+                    'you have to accept our conditions before sending data'),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        this.isAccepted = true;
+                        setState(() {});
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (context) {
+                          return HomePage(
+                            email: this.email,
+                            password: this.password,
+                          );
+                        }));
+                      },
+                      child: Text('Accept')),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('Cancle')),
+                ],
+              );
+            });
+      }
     }
   }
 
@@ -111,46 +128,61 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         title: Text('Login'),
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-            child: TextField(
-              onChanged: (v) {
-                this.email = v;
-              },
-              decoration: InputDecoration(
-                  labelText: 'EMAIL',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10))),
+      body: Form(
+        key: formKey,
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              child: TextFormField(
+                validator: (v) {
+                  if (v.length == 0 || v == null) {
+                    return 'Required Field';
+                  } else if (!v.contains('@')) {
+                    return 'Invalid Email syntax';
+                  }
+                },
+                onSaved: (v) {
+                  this.email = v;
+                },
+                decoration: InputDecoration(
+                    labelText: 'EMAIL',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
+              ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-            child: TextField(
-              obscureText: true,
-              onChanged: (v) {
-                this.password = v;
-              },
-              decoration: InputDecoration(
-                  labelText: 'PASSWORD',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10))),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              child: TextFormField(
+                obscureText: true,
+                validator: (v) {
+                  if (v.length <= 6) {
+                    return 'the password must be larger than 6 letters';
+                  }
+                },
+                onSaved: (v) {
+                  this.password = v;
+                },
+                decoration: InputDecoration(
+                    labelText: 'PASSWORD',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
+              ),
             ),
-          ),
-          CheckboxListTile(
-              title: Text('Accept'),
-              value: isAccepted,
-              onChanged: (v) {
-                this.isAccepted = v;
-                setState(() {});
-              }),
-          ElevatedButton(
-              onPressed: () {
-                sendForm();
-              },
-              child: Text('SEND'))
-        ],
+            CheckboxListTile(
+                title: Text('Accept'),
+                value: isAccepted,
+                onChanged: (v) {
+                  this.isAccepted = v;
+                  setState(() {});
+                }),
+            ElevatedButton(
+                onPressed: () {
+                  sendForm();
+                },
+                child: Text('SEND'))
+          ],
+        ),
       ),
     );
   }
